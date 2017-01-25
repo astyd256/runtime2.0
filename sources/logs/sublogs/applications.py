edff7351bfe2d48ce78413871c988d1c80f97063
Nikolay Grishkov <nikolay.grishkov@vdombox.ru>
2017-01-26 03:28:30 +0700

    Log wrappers and update applications log

--- sources/logs/sublogs/applications.py
+++ sources/logs/sublogs/applications.py
@@ -8,7 +8,9 @@ from ..debuglog import DebugLog
 
 
 NAME = "applications"
-WIDTHS = 21, 22, 22
+WIDTHS = 21, 22
+
+# WIDTHS = 21, 22, 22
 
 
 class ApplicationsLogFormatter(PrefixingLogFormatter):
@@ -17,11 +19,21 @@ class ApplicationsLogFormatter(PrefixingLogFormatter):
         super(ApplicationsLogFormatter, self).__init__(NAME, WIDTHS)
 
     def format(self, timestamp, thread, module, level, user, message):
-        return super(ApplicationsLogFormatter, self).format(module, level, timestamp.strftime(settings.LOGGING_TIMESTAMP), thread, user, message)
+        if user:
+            module = "%s|%s" % (module, user)
+        return super(ApplicationsLogFormatter, self).format(module, level, timestamp.strftime(settings.LOGGING_TIMESTAMP), thread, message)
 
     def parse(self, entry):
-        module, level, timestamp, thread, user, message = super(ApplicationsLogFormatter, self).parse(entry)
+        module, level, timestamp, thread, message = super(ApplicationsLogFormatter, self).parse(entry)
+        module, separator, user = module.partition("|")
         return datetime.datetime.strptime(timestamp, settings.LOGGING_TIMESTAMP), thread, module, level, user, message
+
+    # def format(self, timestamp, thread, module, level, user, message):
+    #     return super(ApplicationsLogFormatter, self).format(module, level, timestamp.strftime(settings.LOGGING_TIMESTAMP), thread, user, message)
+
+    # def parse(self, entry):
+    #     module, level, timestamp, thread, user, message = super(ApplicationsLogFormatter, self).parse(entry)
+    #     return datetime.datetime.strptime(timestamp, settings.LOGGING_TIMESTAMP), thread, module, level, user, message
 
 
 class ApplicationLog(DebugLog):
--- sources/scripting/wrappers/log.py
+++ sources/scripting/wrappers/log.py
@@ -1,11 +1,25 @@
 
-import managers, version, utils
+import managers
 
 
 class VDOM_log(object):
 
-	def debug(self, message, *arguments):
-		print message%arguments
+    def write(self, message, *arguments, **keywords):
+        uuid = managers.engine.application.id
+        user = keywords.get("user")
+        managers.log_manager.logs.application(uuid).write(message % arguments, module=uuid, user=user)
 
-	def error(self, message, *arguments):
-		print message%arguments
+    def debug(self, message, *arguments, **keywords):
+        uuid = managers.engine.application.id
+        user = keywords.get("user")
+        managers.log_manager.logs.application(uuid).debug(message % arguments, module=uuid, user=user)
+
+    def warning(self, message, *arguments, **keywords):
+        uuid = managers.engine.application.id
+        user = keywords.get("user")
+        managers.log_manager.logs.application(uuid).warning(message % arguments, module=uuid, user=user)
+
+    def error(self, message, *arguments, **keywords):
+        uuid = managers.engine.application.id
+        user = keywords.get("user")
+        managers.log_manager.logs.application(uuid).error(message % arguments, module=uuid, user=user)
