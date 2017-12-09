54425b09038b72833e16db8c5d7e49b2c4071f3b
Nikolay Grishkov <nikolay.grishkov@vdombox.ru>
2017-12-09 17:16:36 +0700

    Minify javascript and styles

--- sources/utils/pages.py
+++ sources/utils/pages.py
@@ -8,75 +8,31 @@ from utils.tracing import describe_exception, \
 
 
 PAGE_STYLES = """\
-body
-{
-    margin: 0rem; min-width: 50rem;
-    color: black; background: white;
-    font-family: "Trebuchet MS", Helvetica, sans-serif; font-size: 2rem;
-}
-
-.heading {padding: 1rem 2rem; color: white; background: brown; font-size: 1.5em;}
-.content {padding: 1rem 2rem 2rem 2rem;}
-
-.extra {padding: 1rem 2rem 2rem 2rem;}
-.extra:empty {display: none;}
+body{margin:0;min-width:50rem;color:black;background:white;\
+font-family:"Trebuchet MS",Helvetica,sans-serif;font-size:2rem}\
+.heading{padding:1rem 2rem;color:white;background:brown;font-size:1.5em}\
+.content{padding:1rem 2rem 2rem}\
+.extra{padding:1rem 2rem 2rem}\
+.extra:empty{display:none}\
 """
 
 PAGE_SCRIPTS = """\
-function expand(element) {
-    var item = element.firstChild;
-    while(item && item.nodeType != Node.ELEMENT_NODE)
-        item = item.nextSibling;
-    item.style.display = "none";
-    item = item.nextSibling;
-    while(item && item.nodeType != Node.ELEMENT_NODE)
-        item = item.nextSibling;
-    item.style.display = "block";
-};
-
-function copy(element)
-{
-    if(typeof(element) == "string")
-    {
-        var item = document.createElement("textarea");
-        item.style.position = "fixed";
-        item.style.top = "0"
-        item.style.left = "0"
-        item.style.width = "0"
-        item.style.height = "0"
-        item.style.visible = "hidden";
-        item.value = element;
-        document.body.appendChild(item);
-        try
-        {
-            item.select();
-            document.execCommand("Copy");
-        }
-        finally
-        {
-            document.body.removeChild(item);
-        };
-    }
-    else
-    {
-        var item = element.firstChild;
-        while(item && item.nodeType != Node.ELEMENT_NODE)
-            item = item.nextSibling;
-        item.select();
-        document.execCommand("Copy");
-    };
-};
+function expand(e){\
+for(var t=e.firstChild;t&&t.nodeType!=Node.ELEMENT_NODE;)t=t.nextSibling;\
+for(t.style.display="none",t=t.nextSibling;t&&t.nodeType!=Node.ELEMENT_NODE;)t=t.nextSibling;\
+t.style.display="block"}\
+function copy(e){\
+var t=document.createElement("textarea");\
+t.style.position="fixed",t.style.top="0",t.style.left="0",t.style.width="0",t.style.height="0";\
+t.style.visible="hidden",t.value=e,document.body.appendChild(t);\
+try{t.select(),document.execCommand("Copy")}finally{document.body.removeChild(t)}}\
 """
 
 PAGE_TEMPLATE = """\
 <head>
 <title>{title}</title>
-<style>
-{styles}
-</style>
-<script>
-{scripts}
-</script>
+<style>{styles}</style>
+<script>{scripts}</script>
 </head>
 <body>
 <div class="heading">{heading}</div>
@@ -128,75 +84,32 @@ def compose_page(content, title=None, heading=None, extra=None, styles=None, scr
 
 
 TRACE_STYLES = """\
-.extra {font-family: Consolas, monaco, monospace; font-size: 0.4em;}
-
-.extra .debug
-{
-    margin: 0rem -1rem;
-    overflow: hidden;
-    background: url("data:image/svg+xml;utf8,\
+.extra{font-family:Consolas,monaco,monospace;font-size:.4em}\
+.extra .debug{margin:0 -1rem;overflow:hidden;background:url("data:image/svg+xml;utf8,\
 <svg xmlns='http://www.w3.org/2000/svg' width='32' height='32' viewBox='0 0 10 10'>\
 <path d='M-2,2 l4,-4 M0,10 l10,-10 M8,12 l4,-4' stroke='black' stroke-opacity='0.03' stroke-width='3.5'/>\
-</svg>");
-}
-
-.extra .error
-{
-    position: relative;
-    padding: 0.5rem 3rem 0.5rem 1rem;
-    color: gold; background-color: brown;
-    overflow: hidden; text-overflow: ellipsis; white-space: nowrap;
-    font-weight: bold;
-    background-image: url("data:image/svg+xml;utf8,\
+</svg>")}
+.extra .error{position:relative;padding:.5rem 3rem .5rem 1rem;\
+color:gold;background-color:brown;overflow:hidden;text-overflow:ellipsis;white-space:nowrap;\
+font-weight:bold;background-image:url("data:image/svg+xml;utf8,\
 <svg xmlns='http://www.w3.org/2000/svg' width='32' height='32' viewBox='0 0 10 10'>\
 <path d='M-2,2 l4,-4 M0,10 l10,-10 M8,12 l4,-4' stroke='black' stroke-opacity='0.1' stroke-width='3.5'/>\
-</svg>");
-}
-
-.extra .section {margin: 1rem;}
-
-.extra .cause {color: brown;}
-.extra .cause .items {border-color: brown;}
-
-.extra .locals {color: sienna;}
-.extra .locals .items {border-color: sienna;}
-
-.extra .thread {color: steelblue;}
-.extra .thread .items {border-color: steelblue;}
-
-.extra .other .thread {color: darkcyan;}
-.extra .other .thread .items {border-color: darkcyan;}
-
-.items
-{
-    margin-top: 0.2rem; border-top: 1px dashed gray; padding: 0.2rem 0rem 0rem 1.8rem;
-}
-
-.items > div
-{
-    display: flex;
-    border-radius: 2px; padding: 0rem 0.25rem 0rem 0.25rem;
-    justify-content: stretch; overflow: hidden;
-}
-
-.items > div:hover
-{
-    background: rgba(0, 0, 0, 0.05);
-}
-
-.items > div > div
-{
-    overflow: hidden; text-overflow: ellipsis; white-space: nowrap;
-}
-
-.items > div > div:not(:only-of-type):first-of-type
-{
-    flex: 0 0 35rem;
-}
-
-.items > div > div:not(:only-of-type):not(:last-of-type):after
-{
-    content: "\\00a0\
+</svg>")}\
+.extra .section{margin:1rem}\
+.extra .cause{color:brown}\
+.extra .cause .items{border-color:brown}\
+.extra .locals{color:sienna}\
+.extra .locals .items{border-color:sienna}\
+.extra .thread{color:steelblue}\
+.extra .thread .items{border-color:steelblue}\
+.extra .other .thread{color:darkcyan}\
+.extra .other .thread .items{border-color:darkcyan}\
+.items{margin-top:.2rem;border-top:1px dashed gray;padding:.2rem 0 0 1.8rem}\
+.items > div{display:flex;border-radius:2px;padding:0 .25rem;justify-content:stretch;overflow:hidden}\
+.items > div:hover{background:rgba(0,0,0,0.05)}\
+.items > div > div{overflow:hidden;text-overflow:ellipsis;white-space:nowrap}\
+.items > div > div:not(:only-of-type):first-of-type{flex:0 0 35rem}\
+.items > div > div:not(:only-of-type):not(:last-of-type):after{content:"\\00a0\
 \\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\
 \\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\
 \\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\
@@ -204,37 +117,15 @@ TRACE_STYLES = """\
 \\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\
 \\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\
 \\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\
-\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026";
-}
-
-.items > div > div:not(:only-of-type):not(:first-of-type):before
-{
-    content: "\\00a0";
-}
-
-.items > div > div:not(:only-of-type):not(:first-of-type)
-{
-    overflow: hidden; text-overflow: ellipsis; white-space: nowrap;
-    flex: 1 1 1rem;
-}
-
-.expand
-{
-    margin: calc(-0.25rem - 1px);
-    border: 1px solid rgba(0, 0, 0, 0.075); border-radius: 2px;
-    padding: 0.25rem;
-    background-color: rgba(255, 255, 255, 0.5);
-    cursor: pointer;
-}
-
-.expand:hover
-{
-    border-color: rgba(0, 0, 0, 0); background-color: rgba(0, 0, 0, 0.05);
-}
-
-.copy {position: absolute; right: 1rem; cursor: pointer; font-variant: small-caps;}
-.copy:hover {color: white;}
-
+\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026\\2026"}\
+.items > div > div:not(:only-of-type):not(:first-of-type):before{content:"\\00a0"}\
+.items > div > div:not(:only-of-type):not(:first-of-type){overflow:hidden;text-overflow:ellipsis;\
+white-space:nowrap;\flex:1 1 1rem}\
+.expand{margin:calc(-0.25rem - 1px);border:1px solid rgba(0,0,0,0.075);border-radius:2px;padding:.25rem;\
+background-color:rgba(255,255,255,0.5);cursor:pointer}\
+.expand:hover{border-color:rgba(0,0,0,0);background-color:rgba(0,0,0,0.05)}\
+.copy{position:absolute;right:1rem;cursor:pointer;font-variant:small-caps}\
+.copy:hover{color:white}\
 """
 
 
